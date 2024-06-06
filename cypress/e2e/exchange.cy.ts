5fca57df10a1fc50d21c34c9f0445a63abdd4602
Yauheni Kadyrau <7077634@gmail.com>
2024-06-06 22:05:14 +0300

    test: add test

--- cypress/e2e/exchange.cy.ts
+++ cypress/e2e/exchange.cy.ts
@@ -12,3 +12,17 @@ describe('converter ', () => {
 		cy.get('[name="fromCurrencyInput"]').should('be.visible');
 	});
 });
+describe('converter ', () => {
+	beforeEach(() => {
+		cy.visit('/');
+	});
+
+	it('should open modal', () => {
+		cy.get('[data-testid="open-modal-button"]').click({
+			multiple: true,
+			force: true,
+		});
+
+		cy.get('[name="fromCurrencyInput"]').should('be.visible');
+	});
+});
